4db90d24e68a130c90687e994d60c334251ab4fc
Gopal8586 <gopalharsh8586@gmail.com>
2025-07-02 11:48:15 +0530

    Added CORS support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/images', express.static('images'));
 
